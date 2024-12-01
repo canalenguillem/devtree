940ee9798076eeeb287f3d43f9d1f2b7566c91de
enguillem <enguillem@gmail.com>
2024-12-01 11:31:33 +0100

    crear loyouts

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginView from "./views/LoginView"; //+
 import RegisterView from "./views/RegisterView";
+import AuthLayout from "./Layouts/AuthLayout";
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
+        <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginView />} />
           <Route path="/auth/register" element={<RegisterView />} />
         </Route>
--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 function LoginView() {
   return (
     <>
-      <div>Login View</div>
-
+      <h1>Login</h1>
       <nav>
         <Link to="/auth/register">crear una cuenta</Link>
       </nav>
