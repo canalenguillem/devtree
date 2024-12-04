03a0d4df921e06d53da192f6dce72e65010d0db2
enguillem <enguillem@gmail.com>
2024-12-04 06:09:56 +0100

    añadida navegación

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginView from "./views/LoginView"; //+
 import RegisterView from "./views/RegisterView";
 import AuthLayout from "./Layouts/AuthLayout";
+import AppLayout from "./Layouts/AppLayout";
+import LinkTreeView from "./views/LinkTreeView";
+import ProfileView from "./views/ProfileView";
 function Router() {
   return (
     <BrowserRouter>
@@ -9,6 +12,10 @@ function Router() {
         <Route element={<AuthLayout />}>
           <Route path="/auth/login" element={<LoginView />} />
           <Route path="/auth/register" element={<RegisterView />} />
+        </Route>
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index={true} element={<LinkTreeView />} />
+          <Route path="profile" element={<ProfileView />} />
         </Route>
       </Routes>
     </BrowserRouter>
